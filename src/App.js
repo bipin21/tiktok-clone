8e34e2727740d4cffc11905047acfdb92e648e27
“Bipin <bipinkarki172@gmail.com>
2020-08-23 16:45:18 +0545

    Add video controls

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Tiktok clone</h1>
 
       <div className="app__videos">
         <Video />
@@ -13,6 +12,7 @@ function App() {
         <Video />
         <Video />
       </div>
+
     </div>
   );
 }
--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./Video.css"
 function Video() {
+    const videoRef = useRef(null);
+    const [play, setPlay] = useState(false)
+    const onVideoPress = () => {
+        if(play){
+            videoRef.current.pause();
+            setPlay(false);
+        }else{
+            videoRef.current.play();
+            setPlay(true);
+        }
+    };
+    
+
     return (
         <div className="video">
-            This is video
-            
+            <video
+            className="video__player"
+            src="sample.mp4"
+            loop
+            ref={videoRef}
+            onClick={onVideoPress}
+            >
+
+            </video>
+
             {/* Video Sidebar */}
 
             {/* Video Footer */}
