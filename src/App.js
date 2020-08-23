47fb8c6c36835a5917d75d71ba8283e18d99b663
“Bipin <bipinkarki172@gmail.com>
2020-08-23 16:12:15 +0545

    Add snaps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Video from './Video';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>Tiktok clone</h1>
+
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Video.css"
+function Video() {
+    return (
+        <div className="video">
+            This is video
+            
+            {/* Video Sidebar */}
+
+            {/* Video Footer */}
+        </div>
+    )
+}
+
+export default Video
