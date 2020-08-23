73c66a3653dbbc5da700d4b1396c33edfba1c8f8
Bipin Karki <bipin.karki@itonics.de>
2020-08-23 15:29:02 +0545

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpJ6RQrQc7jCYh3rDIc349UvQxXbchUQ8",
+    authDomain: "tiktok-clone-bbdae.firebaseapp.com",
+    databaseURL: "https://tiktok-clone-bbdae.firebaseio.com",
+    projectId: "tiktok-clone-bbdae",
+    storageBucket: "tiktok-clone-bbdae.appspot.com",
+    messagingSenderId: "10610252595",
+    appId: "1:10610252595:web:acfdaa94790293848efc46",
+    measurementId: "G-70GJB6WYST"
+  };
